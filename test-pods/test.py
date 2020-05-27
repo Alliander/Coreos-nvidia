8552a7ec64689ca403b06ea773eef4693279461e
Hugo van der Zwaag <hugovdzwaag@hotmail.com>
2020-05-27 09:56:51 +0200

    wip kubeflow

--- test-pods/test.py
+++ test-pods/test.py
@@ -1,8 +1,24 @@
 import tensorflow as tf
-with tf.device('/gpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c = tf.matmul(a, b)
+import argparse
 
-with tf.Session() as sess:
-    print (sess.run(c))
+parser = argparse.ArgumentParser(description='My program description')
+parser.add_argument('--input1-path', type=str,
+                    help='Path of the local file containing the Input 1 data.')  # Paths should be passed in, not hardcoded
+parser.add_argument('--output1-path', type=str,
+                    help='Path of the local file where the Output 1 data should be written.')  # Paths should be passed in, not hardcoded
+# parser.add_argument('--param1', type=int, default=100, help='Parameter 1.')
+args = parser.parse_args()
+
+
+def kubeflow_tensor_gpu(input: str):
+    with tf.device('/gpu:0'):
+        a = tf.constant(str, shape=[2, 3], name='a')
+        b = tf.constant(str, shape=[3, 2], name='b')
+        c = tf.matmul(a, b)
+
+    with tf.Session() as sess:
+        with open(args.output1_path, 'w') as out_file:
+            out_file.write(sess.run(c))
+
+
+kubeflow_tensor_gpu(args.input1_path)
